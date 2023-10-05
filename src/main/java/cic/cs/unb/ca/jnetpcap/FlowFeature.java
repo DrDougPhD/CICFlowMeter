60a75733efb80bfb2aa781c9d0548b1961af37d1
Lisa Liu-Thorrold <l.liuthorrold@adfa.edu.au>
2023-10-05 13:16:53 +1100

    Adding total connection time for UDP

--- src/main/java/cic/cs/unb/ca/jnetpcap/FlowFeature.java
+++ src/main/java/cic/cs/unb/ca/jnetpcap/FlowFeature.java
@@ -104,7 +104,7 @@ public enum FlowFeature {
     bwd_tcp_retrans("Bwd TCP Retrans. Count", "BwTcpRt"), //89
     total_tcp_retrans("Total TCP Retrans. Count", "TotalTcpRt"), //90
 
-    cum_tcp_time("Total TCP Flow Time", "TTFT"), //91
+    cum_cnx_time("Total Connection Flow Time", "TCFT"), // 91
 	
 	Label("Label","LBL",new String[]{"NeedManualLabel"});	//92
 
